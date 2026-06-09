fd699b0a35e51c47bc20490ed45e0886ae1e4689
Lara Peddinghausen <larapedd@gmail.com>
2026-06-09 12:56:22 -0300

    feat(assets): Cria módulo IMAGES para gerenciamento centralizado de assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+export const IMAGES = {
+    apartamentoX: require('./apartamentoX.jpg'),
+    logo: require('./logo1.png'),
+    logo2: require('./logo2.png'),
+    logo3: require('./logo3.png'),
+    logo4: require('./logo4.png'),
+    
+};
