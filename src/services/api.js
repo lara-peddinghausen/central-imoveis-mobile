62b7d41f0880bb1bd7ef40f5c14e2e04ec0dfdbd
Lara Peddinghausen <larapedd@gmail.com>
2026-06-09 13:34:07 -0300

    feat(api): configura instâncias do Axios para a API local e ViaCEP

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const apiCorreios = axios.create({
+    baseURL: 'https://viacep.com.br/ws'
+})
+
+export default [api, apiCorreios];
