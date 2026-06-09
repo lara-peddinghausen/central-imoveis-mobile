8a10236f8f02be58f060ad66d230c5e694bd48d8
Lara Peddinghausen <larapedd@gmail.com>
2026-06-09 13:35:14 -0300

    refactor(router): reorganiza o layout global de rotas do Expo Router

--- app/_layout.js
+++ app/_layout.js
@@ -1,29 +1,11 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-    return (<Stack>
-        <Stack.Screen
-            name="login"
-            options={{ headerShown: false }}
-        />
-        <Stack.Screen
-            name='home'
-            // options={{ headerShown: false }}
-            options={{
-                
-                headerShown: true,
-                headerStyle: {
-                    backgroundColor: '#0B3B63'
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold'
-                },
-                headerTitleAlign: 'center'
-            }}
-
-        />
-
-
-    </Stack>);
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="login" />
+            <Stack.Screen name="(tabs)" />
+        </Stack>
+    );
 }
