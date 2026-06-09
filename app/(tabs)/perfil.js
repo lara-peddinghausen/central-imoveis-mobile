b43115dbb86373a7b0f458291387ed8804034b05
Lara Peddinghausen <larapedd@gmail.com>
2026-06-09 20:31:06 -0300

    feat(perfil): desenha tela de perfil com formulário e componentes customizados

--- app/(tabs)/perfil.js
+++ app/(tabs)/perfil.js
@@ -1,6 +1,11 @@
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import InputItem from '../../src/components/InputItem';
+import { COLORS } from '../../src/theme/colors';
+import { FONT_SIZE } from '../../src/theme/typography';
+import { ButtonDark } from '../../src/components/ButtonDark';
+import { ButtonLight } from '../../src/components/ButtonLight';
 
 export default function Perfil() {
     const router = useRouter();
@@ -14,21 +19,91 @@ export default function Perfil() {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Meu Perfil</Text>
-            <Text style={styles.info}>Administrador do Sistema</Text>
-            
-            <View style={styles.buttonContainer}>
-                <Button title="Sair do Aplicativo" onPress={handleLogout} color="#DC3545" />
+
+            <View style={styles.titleArea} >
+                <View style={styles.line} />
+                <Text style={styles.title}>Meu perfil</Text>
+                <View style={styles.line} />
+            </View>
+
+            <View style={styles.formArea}>
+                <InputItem
+                    label='Nome'
+                    placeholder=''
+                    value={''}
+                    keyboardType='numeric'
+                    onChangeText={() => { }}
+                />
+                <InputItem
+                    label='E-mail'
+                    placeholder=''
+                    value={''}
+                    onChangeText={() => { }}
+                />
+                <InputItem
+                    label='Data de nascimento'
+                    placeholder=''
+                    value={''}
+                    onChangeText={() => { }}
+                />
+            </View>
+
+
+
+            <View style={styles.buttonArea}>
+                <ButtonDark
+                    title="Editar"
+                    // onPress={cadastrar}
+                    flex
+                />
+                {/* <ButtonLight
+                    title="Cancelar"
+                    // onPress={cancelar}
+                    flex
+                /> */}
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff', padding: 16 },
-    title: { fontSize: 20, fontWeight: 'bold', color: '#333', marginBottom: 4 },
-    info: { fontSize: 16, color: '#666', marginBottom: 24 },
-    buttonContainer: { width: '80%' }
+    container: {
+        paddingTop: 80,
+        flexGrow: 1,
+        backgroundColor: COLORS.white,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        gap: 20,
+
+    },
+    titleArea: {
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    line: {
+        width: '40%',
+        height: 2,
+        backgroundColor: COLORS.darkBlue,
+        marginHorizontal: 20,
+    },
+    title: {
+        fontSize: FONT_SIZE.xlarge,
+        color: COLORS.darkBlue,
+        fontWeight: 'bold',
+    },
+    buttonArea: {
+        flexDirection: 'row',
+        width: 200
+    },
+    formArea: {
+        borderWidth: 1,
+        borderColor: COLORS.grey,
+        borderRadius: 10,
+        width: '90%',
+        alignItems: 'center',
+        paddingBottom: 15,
+    },
 });
 
-// EXEMPLO
