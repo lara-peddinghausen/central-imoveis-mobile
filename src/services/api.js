f2cc90cf1b9079dffd02a66a7a98d561594d6441
Lara Peddinghausen <larapedd@gmail.com>
2026-06-12 17:06:48 -0300

    fix(api): ajustar baseURL para emulador android e endpoint do viacep

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'http://10.0.2.2:8080', 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 const apiCorreios = axios.create({
-    baseURL: 'https://viacep.com.br/ws'
+    baseURL: 'https://viacep.com.br/ws/'
 })
 
 export { api, apiCorreios };
