0ec3439b531de5f11b5e327c4af655749c1dece9
Lara Peddinghausen <larapedd@gmail.com>
2026-06-16 17:19:20 -0300

    style(theme): atualiza variavel de cor no arquivo colors.js

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,7 +1,7 @@
 export const COLORS = {
     darkBlue: '#0B3B63',
     lightBlue: '#466DC2',
-    babyBlue: 'rgb(205, 223, 255)',
+    babyBlue: '#B5C8F2',
     yellowCDI: '#F4CC68',
     grey: '#8B8B8B',
     lightGrey: '#C8C8C8',
